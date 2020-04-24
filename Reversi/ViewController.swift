85ea72ff60a829c547c4c68902b19f77d7eba063
Teruto Yamasaki <y.teruto@gmail.com>
2020-04-24 22:15:50 +0900

    「Tied」表示をセンタリングするためにトルツメしていた部分はstackviewを利用

--- Reversi/ViewController.swift
+++ Reversi/ViewController.swift
@@ -5,19 +5,9 @@ class ViewController: UIViewController {
     @IBOutlet private var boardView: BoardView!
     @IBOutlet private var messageDiskView: DiskView!
     @IBOutlet private var messageLabel: UILabel!
-    @IBOutlet private var messageDiskSizeConstraint: NSLayoutConstraint!
-
     @IBOutlet private var playerControls: [UISegmentedControl]!
     @IBOutlet private var countLabels: [UILabel]!
     @IBOutlet private var playerActivityIndicators: [UIActivityIndicatorView]!
-
-    /// Storyboard 上で設定されたサイズを保管します。
-    /// 引き分けの際は `messageDiskView` の表示が必要ないため、
-    /// `messageDiskSizeConstraint.constant` を `0` に設定します。
-    /// その後、新しいゲームが開始されたときに `messageDiskSize` を
-    /// 元のサイズで表示する必要があり、
-    /// その際に `messageDiskSize` に保管された値を使います。
-    private var messageDiskSize: CGFloat!
 
     /// どちらの色のプレイヤーのターンかを表します。ゲーム終了時は `nil` です。
     private var turn: Disk = .dark
@@ -30,7 +20,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         boardView.delegate = self
-        messageDiskSize = messageDiskSizeConstraint.constant
         
         do {
             let loadedGame = try GameIO.loadGame(boardView: boardView, playerControls: playerControls)
@@ -83,7 +72,7 @@ class ViewController: UIViewController {
 
     /// 現在のターンをメッセージラベルに表示
     private func updateMessageViews(side: Disk) {
-        messageDiskSizeConstraint.constant = messageDiskSize
+        messageDiskView.isHidden = false
         messageDiskView.disk = side
         messageLabel.text = "'s turn"
     }
@@ -94,12 +83,12 @@ class ViewController: UIViewController {
         let lightCount = boardView.countDisks(of: .light)
         if darkCount == lightCount {
             // 引き分けを表示
-            messageDiskSizeConstraint.constant = 0
+            messageDiskView.isHidden = true
             messageLabel.text = "Tied"
         } else {
             // 勝者を表示
             let winner: Disk = (darkCount > lightCount) ? .dark : .light
-            messageDiskSizeConstraint.constant = messageDiskSize
+            messageDiskView.isHidden = false
             messageDiskView.disk = winner
             messageLabel.text = " won"
         }
