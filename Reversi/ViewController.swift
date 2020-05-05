60dd802158a53e42b6ba6d4d2bb4a6bd181d6c9e
Teruto Yamasaki <y.teruto@gmail.com>
2020-05-06 00:50:18 +0900

    Cancelerをファイル作成

--- Reversi/ViewController.swift
+++ Reversi/ViewController.swift
@@ -233,21 +233,6 @@ extension ViewController: BoardViewDelegate {
     }
 }
 
-final class Canceller {
-    private(set) var isCancelled: Bool = false
-    private let body: (() -> Void)?
-    
-    init(_ body: (() -> Void)?) {
-        self.body = body
-    }
-    
-    func cancel() {
-        if isCancelled { return }
-        isCancelled = true
-        body?()
-    }
-}
-
 // MARK: File-private extensions
 
 extension Disk {
